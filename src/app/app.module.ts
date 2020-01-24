e2de0f6c13a6bf2eec9f789218b47e0821769b75
Addikins <addikinsplays@gmail.com>
2020-01-24 09:39:03 -0700

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule } from '@angular/router';
+import {MatCardModule} from '@angular/material';
+import {MatButtonModule} from '@angular/material';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PrototypeComponent } from './prototype/prototype.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrototypeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+
+    MatCardModule,
+    MatButtonModule,
+    RouterModule.forRoot([
+      { path: '', component: PrototypeComponent, pathMatch: 'full'}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
