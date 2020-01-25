63e00d8daa05e49edf936908ef582e2d4a33af9b
Addikins <addikinsplays@gmail.com>
2020-01-25 15:46:28 -0700

    Add additional cards divided into rows and columns
    Import and use FlexLayoutModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { RouterModule } from '@angular/router';
-import {MatCardModule} from '@angular/material';
-import {MatButtonModule} from '@angular/material';
+import { RouterModule } from "@angular/router";
+import { MatCardModule } from "@angular/material";
+import { MatButtonModule } from "@angular/material";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { PrototypeComponent } from './prototype/prototype.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { PrototypeComponent } from "./prototype/prototype.component";
 
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PrototypeComponent
-  ],
+  declarations: [AppComponent, PrototypeComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
 
+    FlexLayoutModule,
     MatCardModule,
     MatButtonModule,
     RouterModule.forRoot([
-      { path: '', component: PrototypeComponent, pathMatch: 'full'}
+      { path: "", component: PrototypeComponent, pathMatch: "full" }
     ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/prototype/prototype.component.ts
+++ src/app/prototype/prototype.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { concat } from 'rxjs';
 
 @Component({
-  selector: 'app-prototype',
-  templateUrl: './prototype.component.html',
-  styleUrls: ['./prototype.component.css']
+  selector: "app-prototype",
+  templateUrl: "./prototype.component.html",
+  styleUrls: ["./prototype.component.css"]
 })
 export class PrototypeComponent implements OnInit {
+  public myString: string = "A Simple String";
+  public columns: number[] = [0, 1, 2, 3, 4];
+  public rows: number[] = [0, 1, 2, 3, 4, 5, 6];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  onChange(passedText: string) {
+    this.myString += passedText;
   }
-
 }
